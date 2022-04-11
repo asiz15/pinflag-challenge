3b8ef8388106d6a4af29eb0ac26f819a158e1d9e
asiz <a.silvazapata@gmail.com>
2022-04-10 21:57:07 -0400

    Update: Created "characters" and "characters routes"

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import app from 'express'
 
 import Home from './home_routes'
 import Character from './character_routes'
+import Characters from './characters_routes'
 
 const routes = app.Router()
 
 routes.use('/', Home)
+routes.use('/characters', Characters)
 routes.use('/character', Character)
 
 export default routes
