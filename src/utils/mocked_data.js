3c14c2330f062ee1b6ad39eb84fb8f1375e1a710
asiz <a.silvazapata@gmail.com>
2022-04-10 22:00:40 -0400

    Tests: Added mocked data

---
+++ src/utils/mocked_data.js
@@ -0,0 +1,73 @@
+
+export const defaultCharacter = {
+  name: 'Default character',
+  species: 'Default specie',
+  origin: 'Default origin',
+  status: 'Default status'
+}
+
+export const shortNameCharacter = {
+  name: 'De',
+  species: 'Default specie',
+  origin: 'Default origin',
+  status: 'Default status'
+}
+
+export const shortSpeciesCharacter = {
+  name: 'Deas',
+  species: '',
+  origin: 'Default origin',
+  status: 'Default status'
+}
+
+export const shortOriginCharacter = {
+  name: 'Deasa1',
+  species: 'Specieeee',
+  origin: 'o',
+  status: 'Default status'
+}
+
+export const shortStatusCharacter = {
+  name: 'Deasa1',
+  species: 'Specieeee',
+  origin: 'originngg',
+  status: ''
+}
+
+export const shortCharacter = {
+  name: 'D',
+  species: 'Sp',
+  origin: 'o',
+  status: ''
+}
+
+export const randomCharacter = {
+  name: 'Random character',
+  species: 'Random specie',
+  origin: 'Random origin',
+  status: 'Random status'
+}
+
+export const characterWithoutStatus = {
+  name: 'Random character',
+  species: 'Random specie',
+  origin: 'Random origin'
+}
+
+export const characterWithoutOrigin = {
+  name: 'Random character',
+  species: 'Random specie',
+  status: 'Random status'
+}
+
+export const characterWithoutSpecies = {
+  name: 'Random character',
+  origin: 'Random origin',
+  status: 'Random status'
+}
+
+export const characterWithoutName = {
+  species: 'Random specie',
+  origin: 'Random origin',
+  status: 'Random status'
+}
