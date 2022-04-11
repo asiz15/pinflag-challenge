ee9cb88b8dffaf191cfa26fd9e99d654465080bd
asiz <a.silvazapata@gmail.com>
2022-04-10 21:53:51 -0400

    Update: Created new route '/character'

--- src/routes/character_routes.js
+++ src/routes/character_routes.js
@@ -1,8 +1,18 @@
 import app from 'express'
-import asyncHandler from 'express-async-handler'
-
+import { query, checkSchema } from 'express-validator'
 import CharacterController from '../controllers/character_controller'
+import { charactersByNameMiddleware, createCharacterMiddleware } from '../middlewares'
+import { characterSchema } from '../utils/schemas'
 
 const routes = app.Router()
+
+// Se extrae a una ruta nombrada en singular.
+// Cuando se crea un "character" a través del método POST, hace referencia explícita a 1 "character"
+// Algo similar a cuando se busca por nombre, aunque la respuesta pueda incluir multiples resultados
+// Creí que sería una buena idea 😃
+
+// Validación a través de middlewares usando express-validator (src/middlewares)
+routes.get('/', query('name').exists().isString(), charactersByNameMiddleware, new CharacterController().index)
+routes.post('/', checkSchema({ ...characterSchema }), createCharacterMiddleware, new CharacterController().create)
 
 export default routes
